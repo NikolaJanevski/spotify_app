a059a6cffa4f803e74eb8ec0958bc3163f3db643
NikolaJanevski <njanevsk@mix.wvu.edu>
2021-09-12 14:21:58 -0400

    Added a navigation bar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './header/topbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule
