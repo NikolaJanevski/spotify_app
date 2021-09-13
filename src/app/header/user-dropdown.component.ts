008b5fb1787e14bd4ddece1b0fdfce73af9b2f62
NikolaJanevski <njanevsk@mix.wvu.edu>
2021-09-12 21:22:40 -0400

    Added bootstrap icons and some styling to buttons.

--- src/app/header/user-dropdown.component.ts
+++ src/app/header/user-dropdown.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "spotify-user-dropdown",
-    templateUrl: "user-dropdown.component.html"
+    templateUrl: "user-dropdown.component.html",
+    styleUrls: ["user-dropdown.component.css"]
 })
 export class UserDropdownComponent {
 
