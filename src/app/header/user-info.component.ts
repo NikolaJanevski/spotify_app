e81383b3b2087f5a4dffeeb6215344590b32f47b
NikolaJanevski <njanevsk@mix.wvu.edu>
2021-10-14 18:41:08 -0400

    UserInfoService added

--- src/app/header/user-info.component.ts
+++ src/app/header/user-info.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from "@angular/common/http";
 import { Component, Injectable, OnInit } from "@angular/core";
 import { UserInfo } from "./user-info.model";
+import { UserInfoService } from "./user-info.service";
 
-@Injectable()
 @Component({
     selector: "spotify-user-info",
     templateUrl: "user-info.component.html"
@@ -10,20 +9,15 @@ import { UserInfo } from "./user-info.model";
 export class UserInfoComponent implements OnInit {
     myInfo: UserInfo | undefined;
 
-    constructor(private http: HttpClient) {
+    constructor(private userInfoService:UserInfoService) {
     }
     ngOnInit(): void {
-        console.log("Sending a get request to the server");
-        this.getUserInfo();
         console.log("Registering showUserInfo as a subscriber");
         this.showUserInfo();
     }
 
-    getUserInfo() {
-        return this.http.get<UserInfo>('https://spotify-app-4716b-default-rtdb.firebaseio.com/my-info.json');
-    }
     showUserInfo() {
-        this.getUserInfo().subscribe((data: UserInfo) => {
+        this.userInfoService.getUserInfo().subscribe((data: UserInfo) => {
             console.log(data);
             this.myInfo = data;
         })
---
+++ src/app/header/user-info.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { UserInfo } from "./user-info.model";
+
+@Injectable({providedIn: 'root'})
+export class UserInfoService {
+    private baseUrl:string = 'https://spotify-app-4716b-default-rtdb.firebaseio.com/';
+    private myInfoEndpoint:string = 'my-info.json';
+    
+    constructor(private http: HttpClient) {
+
+    }
+
+    getUserInfo() {
+        console.log(this.baseUrl + this.myInfoEndpoint);
+        return this.http.get<UserInfo>(this.baseUrl + this.myInfoEndpoint);
+    }
+}
